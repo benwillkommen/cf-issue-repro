f5dcfa263d1a88a5bd28e261085d6e6fc376ca62
Ben Willkommen <bwillkommen@uship.com>
2018-12-19 15:08:14 -0600

    attempted to use lodash in a worker using webpack: true in serverless.yml. it worked!

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,10 @@
+const _ = require('lodash');
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
   })
   
   async function handleRequest(request) {
-    return new Response("Hello world")
+    const message = _.join(["Hello world", "using lodash"], "");
+    return new Response(message);
   }
